1dbf3aae19914c10ddb5672026eb94480d6232ed
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2025-06-24 15:49:08 -0400

    yeah

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,18 +1,21 @@
 package;
 
+import flixel.util.FlxColor;
 import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.FlxG;
 import flixel.FlxState;
 
 class PlayState extends FlxState {
 	var dumb:FlxSprite3D;
+
 	override public function create() {
 		super.create();
 		dumb = new FlxSprite3D();
-		dumb.frames = FlxAtlasFrames.fromSparrow("assets/images/DADDY_DEAREST.png", "assets/images/DADDY_DEAREST.xml");
-		for(anim in ["idle", "singDOWN", "singUP", "singLEFT", "singRIGHT"]){
+		/*/dumb.frames = FlxAtlasFrames.fromSparrow("assets/images/DADDY_DEAREST.png", "assets/images/DADDY_DEAREST.xml");
+		for (anim in ["idle", "singDOWN", "singUP", "singLEFT", "singRIGHT"]) {
 			dumb.animation.addByPrefix(anim, anim, 24, false);
-		}
+		}*/
+		dumb.makeGraphic(400, 400, FlxColor.WHITE);
 		dumb.screenCenter();
 		dumb.antialiasing = true;
 		add(dumb);
@@ -21,23 +24,26 @@ class PlayState extends FlxState {
 	override public function update(elapsed:Float) {
 		super.update(elapsed);
 		dumb.angle3D.x = (FlxG.mouse.viewY / FlxG.height) * 180 - 90;
-		dumb.angle3D.y = -(FlxG.mouse.viewX / FlxG.width) * 180 + 90;
+		dumb.angle3D.y = -(FlxG.mouse.viewX / FlxG.width)* 180 + 90;
+		dumb.angle3D.z += FlxG.mouse.wheel;
 		/*dumb.angle3D.x = Math.cos(FlxG.game.ticks / 1000) * 45;
-		dumb.angle3D.y = Math.sin(FlxG.game.ticks / 1000) * 45;*/
-		if(FlxG.keys.pressed.UP){
+			dumb.angle3D.y = Math.sin(FlxG.game.ticks / 1000) * 45; */
+		/*if (FlxG.keys.pressed.UP) {
 			dumb.animation.play("singUP", false);
-		}
-		else if(FlxG.keys.pressed.DOWN){
+		} else if (FlxG.keys.pressed.DOWN) {
 			dumb.animation.play("singDOWN", false);
-		}
-		else if(FlxG.keys.pressed.RIGHT){
+		} else if (FlxG.keys.pressed.RIGHT) {
 			dumb.animation.play("singRIGHT", false);
-		}
-		else if(FlxG.keys.pressed.LEFT){
+		} else if (FlxG.keys.pressed.LEFT) {
 			dumb.animation.play("singLEFT", false);
-		}
-		else{
+		} else {
 			dumb.animation.play("idle", false);
+		}*/
+		if(FlxG.keys.pressed.E){
+			FlxG.camera.zoom += elapsed;
+		}
+		if(FlxG.keys.pressed.Q){
+			FlxG.camera.zoom -= elapsed;
 		}
 	}
 }
